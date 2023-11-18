a23c72218dadc9057069990abba2cb52dfac6d72
huangqiang <2012wong.q@gmail.com>
2023-11-18 19:18:05 +0800

    修改引用生成

--- Assets/Script_AOT/Editor/BuildTool.cs
+++ Assets/Script_AOT/Editor/BuildTool.cs
@@ -7,6 +7,7 @@ using System.Text;
 using HybridCLR.Editor;
 using HybridCLR.Editor.Commands;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 using YooAsset.Editor;
 
@@ -87,7 +88,7 @@ namespace Script_AOT.Editor
             PrebuildCommand.GenerateAll();
             //生成linkFile
             var xmlPath = Application.dataPath + "/HybridCLRGenerate/link.xml";
-            BuildLinkFile.GenerateLinkfile(xmlPath);
+            BuildLinkFilev2.GenerateLinkfile(xmlPath);
             //热更新dll
             CompileDllCommand.CompileDllActiveBuildTarget();
             var target = EditorUserBuildSettings.activeBuildTarget;
@@ -225,7 +226,7 @@ namespace Script_AOT.Editor
     }
 
 
-    public static class BuildLinkFile
+    public static class BuildLinkFilev2
     {
         private static string _il2cppManagedPath = string.Empty;
 
@@ -252,24 +253,30 @@ namespace Script_AOT.Editor
                         case BuildTarget.Android:
                             extendPath = "PlaybackEngines/AndroidPlayer/Variations/il2cpp/Managed/";
                             break;
+                        case BuildTarget.WebGL:
+                            extendPath = "PlaybackEngines/WebGLSupport/Variations/nondevelopment/Data/Managed/";
+                            break;
                         default:
                             throw new Exception($"[BuildPipeline::GenerateLinkfile] 请选择合适的平台, 目前是:{buildTarget}");
                     }
 #elif UNITY_EDITOR_OSX
-                    switch (buildTarget)
-                    {
-                        case BuildTarget.StandaloneOSX:
-                            extendPath = "PlaybackEngines/MacStandaloneSupport/Variations/il2cpp/Managed/";
-                            break;
-                        case BuildTarget.iOS:
-                            extendPath = "../../PlaybackEngines/iOSSupport/Variations/il2cpp/Managed/";
-                            break;
-                        case BuildTarget.Android:
-                            extendPath = "../../PlaybackEngines/AndroidPlayer/Variations/il2cpp/Managed/";
-                            break;
-                        default:
-                            throw new Exception($"[BuildPipeline::GenerateLinkfile] 请选择合适的平台, 目前是:{buildTarget}");
-                    }
+                switch (buildTarget)
+                {
+                    case BuildTarget.StandaloneOSX:
+                        extendPath = "PlaybackEngines/MacStandaloneSupport/Variations/il2cpp/Managed/";
+                        break;
+                    case BuildTarget.iOS:
+                        extendPath = "../../PlaybackEngines/iOSSupport/Variations/il2cpp/Managed/";
+                        break;
+                    case BuildTarget.Android:
+                        extendPath = "../../PlaybackEngines/AndroidPlayer/Variations/il2cpp/Managed/";
+                        break;                        
+                    case BuildTarget.WebGL:
+                        extendPath = "PlaybackEngines/WebGLSupport/Variations/nondevelopment/Data/Managed/";
+                        break;
+                    default:
+                        throw new Exception($"[BuildPipeline::GenerateLinkfile] 请选择合适的平台, 目前是:{buildTarget}");
+                }
 #endif
                     if (string.IsNullOrEmpty(extendPath))
                     {
@@ -285,16 +292,11 @@ namespace Script_AOT.Editor
 
         private static List<string> IgnoreClass = new()
         {
-            "editor", "netstandard", "Bee.", "dnlib", ".framework", "Test", "plastic", "Gradle", "log4net", "Analytics",
-            "System.Drawing",
-            "NVIDIA", "VisualScripting", "UIElements", "IMGUIModule", ".Cecil", "GIModule", "GridModule",
-            "HotReloadModule",
-            "StreamingModule",
-            "TLSModule", "XRModule", "WindModule", "VRModule", "VirtualTexturingModule", "compiler", "BuildProgram",
-            "NiceIO", "ClothModule",
-            "VFXModule", "ExCSS", "GeneratedCode", "mscorlib", "System", "SyncToolsDef"
+            "editor", "netstandard", "Bee.", "dnlib", ".framework", "Test", "plastic", "Gradle", "log4net", "Analytics", "System.Drawing",
+            "NVIDIA", "VisualScripting", "UIElements", "IMGUIModule", ".Cecil", "GIModule", "GridModule", "HotReloadModule", "StreamingModule",
+            "TLSModule", "XRModule", "WindModule", "VRModule", "VirtualTexturingModule", "compiler", "BuildProgram", "NiceIO", "ClothModule",
+            "VFXModule", "ExCSS", "GeneratedCode", "mscorlib", "System", "SyncToolsDef", "ReportGeneratorMerged"
         };
-
         private static bool IsIngoreClass(string classFullName)
         {
             var tmpName = classFullName.ToLower();
@@ -313,7 +315,6 @@ namespace Script_AOT.Editor
         {
             "jetbrain", "editor", "PrivateImplementationDetails", "experimental", "microsoft.", "compiler"
         };
-
         private static bool IsIgnoreType(string typeFullName)
         {
             var tmpName = typeFullName.ToLower();
@@ -350,6 +351,7 @@ namespace Script_AOT.Editor
             foreach (var file in files)
             {
                 var ass = Assembly.LoadFile(file);
+                if (ass != null)
                 {
                     var name = ass.GetName().Name;
                     if (IsIngoreClass(name))
@@ -377,42 +379,40 @@ namespace Script_AOT.Editor
                     continue;
                 }
 
-                if (ass.Location == "")
+                try
                 {
-                    Debug.LogWarning(ass);
-                    continue;
-                }
-
-                Debug.Log(ass + "___" + ass.Location);
-                var assPath = Path.GetFullPath(ass.Location).Replace('\\', '/');
-                if (assPath.Contains(localPath) && assPath.ToLower().Contains("/editor/"))
-                {
-                    continue;
-                }
-
-                var name = ass.GetName().Name;
-                if (hashAss.Contains(name))
-                {
-                    continue;
-                }
-
-                var ignore = false;
-                foreach (var n in names)
-                {
-                    if (name.Contains(n))
+                    var assPath = Path.GetFullPath(ass.Location).Replace('\\', '/');
+                    if (assPath.Contains(localPath) && assPath.ToLower().Contains("/editor/"))
                     {
-                        ignore = true;
-                        break;
+                        continue;
                     }
-                }
 
-                if (ignore)
+                    var name = ass.GetName().Name;
+                    if (hashAss.Contains(name))
+                    {
+                        continue;
+                    }
+
+                    var ignore = false;
+                    foreach (var n in names)
+                    {
+                        if (name.Contains(n))
+                        {
+                            ignore = true;
+                            break;
+                        }
+                    }
+                    if (ignore)
+                    {
+                        continue;
+                    }
+
+                    hashAss.Add(name);
+                    AllAssemblies[name] = ass;
+                }
+                catch (Exception ex)
                 {
-                    continue;
                 }
-
-                hashAss.Add(name);
-                AllAssemblies[name] = ass;
             }
 
             var fullPreserve = new List<string>();
@@ -490,13 +490,300 @@ namespace Script_AOT.Editor
                     writer.WriteAttributeString("preserve", "all");
                     writer.WriteEndElement();
                 }
-
                 writer.WriteEndElement();
             }
 
             writer.WriteEndElement();
             writer.WriteEndDocument();
             writer.Close();
+
+            checkass(Path.Combine(Application.dataPath, "Plugins", "HybridCLR", "Generated", "ReservedAssembly.cs"));
+        }
+
+        public class AssemblyComp : IComparable
+        {
+            public int CompareTo(object obj)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class AsmDefHeader
+        {
+            public string name;
+            public List<string> includePlatforms;
+            public List<string> defineConstraints;
+        }
+
+        private static List<AsmDefHeader> GetAssemblyDefinitionAsset()
+        {
+            var ret = new List<AsmDefHeader>();
+            string[] folderContents = AssetDatabase.FindAssets("t:AssemblyDefinitionAsset");
+
+            foreach (var asset in folderContents)
+            {
+                var tmp = AssetDatabase.LoadAssetAtPath<AssemblyDefinitionAsset>(AssetDatabase.GUIDToAssetPath(asset));
+                if (tmp != null)
+                {
+                    var jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<AsmDefHeader>(tmp.text);
+                    if (jsonObj != null && jsonObj.includePlatforms != null)
+                    {
+                        if (jsonObj.includePlatforms.Count == 0)
+                        {
+                            if (jsonObj.defineConstraints == null || jsonObj.defineConstraints.Count == 0)
+                            {
+                                ret.Add(jsonObj);
+                            }
+                        }
+                        else
+                        {
+                            var hasEditor = false;
+                            foreach (var p in jsonObj.includePlatforms)
+                            {
+                                if (p.ToLower() == "editor")
+                                {
+                                    hasEditor = true;
+                                    break;
+                                }
+                            }
+
+                            if (hasEditor)
+                            {
+                                continue;
+                            }
+
+                            if (jsonObj.defineConstraints == null || jsonObj.defineConstraints.Count == 0)
+                            {
+                                ret.Add(jsonObj);
+                            }
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+
+        private static void checkass(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            var codeTemplate = @"using System.Text;
+using UnityEngine;
+
+namespace GameMain.Scripts.HybridCLR
+{
+    public class ReservedAssembly : MonoBehaviour
+	{
+	    private void Awake()
+		{
+            var sb = new StringBuilder();
+
+			void Reserved<T>()
+			{
+				sb.AppendLine(typeof(T).ToString());
+			}
+
+//Replace This
+            Debug.Log(sb.ToString());
+		}
+	}
+}";
+            var assemblyDefinitionAssets = GetAssemblyDefinitionAsset();
+
+            var watchAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var successAssemblies = new Dictionary<string, Assembly>();
+            foreach (var wa in watchAssemblies)
+            {
+				if (wa.IsDynamic)
+                {
+                    continue;
+                }
+				
+                var locName = Path.GetFileName(wa.Location).ToLower();
+                if (successAssemblies.ContainsKey(locName))
+                {
+                    continue;
+                }
+
+                foreach (var ada in assemblyDefinitionAssets)
+                {
+                    if (locName == ada.name.ToLower() + ".dll")
+                    {
+                        successAssemblies.Add(locName, wa);
+                        break;
+                    }
+                }
+            }
+
+            var assCsharp = watchAssemblies.First(a => a.GetName().Name == "HotUpdate");
+            if (assCsharp != null)
+            {
+                successAssemblies.Add("HotUpdate.dll", assCsharp);
+            }
+
+            var distAssembly = new Dictionary<string, Assembly>();            
+            foreach (var sa in successAssemblies)
+            {
+                var ass = sa.Value;
+                if (ass.IsDynamic)
+                {
+                    continue;
+                }
+
+                if (ass.FullName.Contains("Newtonsoft"))
+                {
+                    continue;
+                }
+
+                if (ass.FullName.Contains("Editor"))
+                {
+                    continue;
+                }
+
+                var hasEditor = false;
+                var ras = ass.GetReferencedAssemblies();
+                foreach (var r in ras)
+                {
+                //    if (r.Name.Contains("UnityEditor"))
+                    {
+                 //       hasEditor = true;
+               //         break;
+                    }
+                }
+
+                if (hasEditor)
+                {
+         //           continue;
+                }
+
+                if (!distAssembly.ContainsKey(ass.FullName))
+                {
+                    distAssembly.Add(ass.FullName, ass);
+                }
+
+                foreach (var r in ras)
+                {
+                    if (r.Name.Contains("UnityEditor"))
+                    {
+                        continue;
+                    }
+
+                    foreach (var wa in watchAssemblies)
+                    {
+                        if (wa.FullName == r.FullName && !distAssembly.ContainsKey(wa.FullName))
+                        {
+                            distAssembly.Add(wa.FullName, wa);
+                        }
+                    }
+                }
+            }
+
+            var info = new Dictionary<string, string>();
+            foreach (var _ass in distAssembly.Values)
+            {
+                var ass = _ass;
+                if (ass.IsDynamic)
+                {
+                    continue;
+                }
+
+                if (ass.FullName.Contains("Assembly-CSharp") 
+                    || ass.FullName.Contains("TestRunner")
+                    || ass.FullName.Contains("HybridCLR")
+                    || ass.FullName.Contains("nunit")
+                    )
+                {
+                    continue;
+                }               
+
+                Debug.Log($"{ass.FullName}");
+
+                var files = Directory.GetFiles(il2cppManagedPath, Path.GetFileName(ass.Location), SearchOption.AllDirectories);
+                if (files.Length > 0)
+                {
+                    ass = Assembly.LoadFile(files[0]);
+                }
+
+                var classOk = false;
+                var allTypes = ass.GetExportedTypes();
+                foreach (var type in allTypes)
+                {
+                    if (!type.IsPublic || type.IsAbstract || type.IsGenericType)
+                    {
+                        continue;
+                    }
+
+                    var name = type.FullName;
+                    if (name.Contains("System.") && ass.FullName.Contains("Newt"))
+                    {
+                        continue;
+                    }
+
+                    var atts = new List<CustomAttributeData>(type.CustomAttributes.Where(a => a.AttributeType.Name.Contains("Obsolete")));
+                    if (atts.Count > 0)
+                    {
+                        continue;
+                    }
+
+                    if (classOk)
+                    {
+                        break;
+                    }
+
+                    var con = type.GetConstructors();
+                    if (con != null && con.Length > 0)
+                    {
+                        foreach (var c in con)
+                        {
+                            if (c.IsConstructor && c.IsPublic)
+                            {
+                                Debug.Log("con true:" +type.FullName);
+                                if (info.TryGetValue(type.FullName, out var val))
+                                {
+                                    Debug.LogError($"{type.FullName} {val} {ass.Location}");
+                                }
+                                else
+                                {
+
+                                    info.Add(type.FullName, ass.Location);
+                                }
+                                classOk = true;
+                                break;
+                            }
+                            else
+                            {
+                                Debug.Log("con false:" +type.FullName);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            var sb = new StringBuilder();
+            var keys = new HashSet<string>(info.Keys);
+            foreach (var k in keys)
+            {
+                var x = info[k];
+                if (k is "UnityEngine.InputManagerEntry")
+                {
+                    continue;
+                }
+                
+                if (Path.GetFileName(x) is "HotUpdate.dll")
+                {
+                    continue;
+                }
+                sb.AppendLine($"\t\t\tReserved<{k}>(); // {Path.GetFileName(x)}");
+            }
+
+            var temp = "System.Text.RegularExpressions.Regex";
+            sb.AppendLine($"\t\t\tReserved<{temp}>(); // {Path.GetFileName(temp)}");
+
+            File.WriteAllText(fileName, codeTemplate.Replace("//Replace This", sb.ToString()));
         }
     }
 }
